6bb178e5b96ef07aabd4ee981e24b681a91b127f
2HenryCardenas1 <cardenashenryesteban@gmail.com>
2023-07-31 16:10:32 -0500

    Queries to Product created

---
+++ src/main/java/com/h3c4/market/persintence/crud/ProductCrudRepository.java
@@ -0,0 +1,21 @@
+package com.h3c4.market.persintence.crud;
+
+import com.h3c4.market.persintence.entity.Producto;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ProductCrudRepository extends CrudRepository<Producto,Integer> {
+
+
+    //@Query(value = "SELECT * FROM productos WHERE id_categoria = ?", nativeQuery = true)
+    List<Producto> findByIdCategoriaOrerByNombreAsc(int idCategoria);
+
+    //Productos que se estan agotando y su estado
+    Optional<List<Producto>> findByCantidadStockLessThanAndEstado(int cantidadStock, boolean estado);
+
+
+
+}
